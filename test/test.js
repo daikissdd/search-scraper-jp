273256da7bfd48469c20619e246cdbd240ac7336
daikissdd <daikissdd@gmail.com>
2015-12-03 16:55:35 +0900

    add test settings

--- test/test.js
+++ test/test.js
@@ -1,10 +1,24 @@
+'use strict';
+
+var expect = require('chai').expect;
+
 var scraper = require('../index');
-
-scraper.yahoo("マーガリンの塗り方", 2, (err, result) => {
-	console.log(result);
-});
-
-var url = 'https://github.com/daikissdd/search-scraper-jp';
-scraper.page(url, (err, result) => {
-	console.log(result);
+describe('search-scraper-jp', () => {
+	it('yahoo scraper', (done) => {
+		scraper.yahoo("マーガリンの塗り方", 2, (err, result) => {
+			expect(result.searchInfo.num).to.be.a('number');
+			expect(result.searchInfo.allInTitleNum).to.be.a('number');
+			expect(result.searchInfo.searchWord).to.be.a('string');
+			expect(result.searchResults.length).to.be.a('number');
+			done();
+		});
+	});
+	it('page scraper', (done) => {
+		var url = 'https://github.com/daikissdd/search-scraper-jp';
+		scraper.page(url, (err, result) => {
+			expect(result.text).to.be.a('string');
+			expect(result.title).to.be.a('string');
+			done();
+		});
+	});
 });
